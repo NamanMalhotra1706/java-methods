55f0aa43dca5e05166839d8f9533601bba320ec0
Naman Malhotra <naman0913.be21@chitkara.edu.in>
2025-03-04 13:02:50 +0530

    [Naman Malhotra] MODIFY: Comments added

--- SimpleInterest.java
+++ SimpleInterest.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class SimpleInterest {
 
+    //Method to calculate and print simple interest
     public static void simpleInterest(int principal, int rate, int time){
         int interest = (principal*rate*time)/100;
         System.out.println("The Simple Interest is " + interest +" for Principal "+ principal+", Rate of Interest " + rate +" and Time " + time );
@@ -9,15 +10,19 @@ public class SimpleInterest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter the principal: ");
-        int principal = sc.nextInt();
-
-        System.out.print("Enter the rate: ");
-        int rate = sc.nextInt();
-
-        System.out.print("Enter the time: ");
-        int time = sc.nextInt();
-
-        simpleInterest(principal,rate,time);
+         // Taking principal amount as input
+         System.out.print("Enter the principal: ");
+         int principal = sc.nextInt();
+ 
+         // Taking rate of interest as input
+         System.out.print("Enter the rate: ");
+         int rate = sc.nextInt();
+ 
+         // Taking time period as input
+         System.out.print("Enter the time: ");
+         int time = sc.nextInt();
+ 
+         // Calling the method to calculate and display the simple interest
+         simpleInterest(principal, rate, time);
     }
 }
